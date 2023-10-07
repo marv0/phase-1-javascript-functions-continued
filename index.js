5eca9c57ce5a0ba6041f4698431d39368c05d86b
Marvin Ogot <marvin.ogot@gmail.com>
2023-10-07 17:55:38 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+//Saturday Fun
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(mondayActivity = "go to the office"){
+    return `This Monday, I will ${mondayActivity}.`;
+}
+
+function wrapAdjective(myText="*"){
+    return function(status="special"){
+        return `You are ${myText}${status}${myText}!`;
+    }
+}
+wrapAdjective("%")("a dedicated programmer");
